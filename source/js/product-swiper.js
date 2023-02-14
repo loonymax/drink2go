99ab11e05cc47d0e1fba364e9b4d258632848af6
Алена <lunymax@gmail.com>
2023-02-14 21:09:36 +0300

    add js for a

--- source/js/product-swiper.js
+++ source/js/product-swiper.js
@@ -1,20 +1,14 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.product', {
   direction: 'horizontal',
   loop: true,
 
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
   },
 });
--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,4 @@
 /* в этот файл добавляет скрипты*/
 import './header-menu.js';
-// import './product-swiper.js';
+import './product-swiper.js';
 import './map.js';
